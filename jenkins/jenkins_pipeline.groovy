1542b927c2530dfdbfcb95f3dac9f0de2793a5fe
broven <broven@163.com>
2020-03-08 23:05:08 +0800

    [jenkins] add code download for pipeline

--- jenkins/jenkins_pipeline.groovy
+++ jenkins/jenkins_pipeline.groovy
@@ -1,14 +1,19 @@
-node {
+node{
+    stage('Source') {
+        git branch: 'master', url: 'git@github.com:brovensmile/BROVEN_V100R001C00.git'
+    }  
+
     /* Requires the Docker Pipeline plugin to be installed */
+     
     sh '''
-         whoami
+        whoami
     '''
+    
     docker.image('node:7-alpine').inside('-v /opt/:/opt') {
-        stage('Test') {
-            sh '''node --version
-                  sleep 300
-            '''
-            
+    stage('Test') {
+        sh '''node --version
+            sleep 300
+        '''
         }
     }
 }
