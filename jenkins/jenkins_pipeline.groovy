b78f608e59ef41f8de6aa756313428646bab7f05
broven <broven@163.com>
2020-03-09 18:35:22 +0800

    [jenkins] add code download for pipeline

--- jenkins/jenkins_pipeline.groovy
+++ jenkins/jenkins_pipeline.groovy
@@ -2,7 +2,7 @@ node{
     stage('Source') {
         //git branch: 'master', url: 'git@github.com:brovensmile/BROVEN_V100R001C00.git'
         //git url: 'https://github.com/jglick/simple-maven-project-with-tests.git'
-        checkout  changelog:true, clearWorkspace:true,  scm:[
+        checkout  changelog:true, poll:true,clearWorkspace:true,scm:[
                  $class: 'GitSCM', 
                  branches: [[name: '*/master']], 
                  userRemoteConfigs: [[url: 'git@github.com:brovensmile/BROVEN_V100R001C00.git']]
