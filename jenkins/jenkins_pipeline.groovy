d1fda2770a71fc3525610d8c65660b298d66f623
broven <broven@163.com>
2020-03-11 01:52:31 +0800

    [jenkins] add workspace config for pipeline

--- jenkins/jenkins_pipeline.groovy
+++ jenkins/jenkins_pipeline.groovy
@@ -2,13 +2,14 @@ node{
     stage('Source') {
         //git branch: 'master', url: 'git@github.com:brovensmile/BROVEN_V100R001C00.git'
         //git url: 'https://github.com/jglick/simple-maven-project-with-tests.git'
+        ws('BROVEN_V100R001C00'){
         checkout  changelog:true, poll:true, scm:[
                  $class: 'GitSCM', 
                  branches: [[name: '*/master']], 
                  userRemoteConfigs: [[url: 'git@github.com:brovensmile/BROVEN_V100R001C00.git']],
                  ]
-    }  
-
+        }  
+    }
     /* Requires the Docker Pipeline plugin to be installed */
      
     sh '''
